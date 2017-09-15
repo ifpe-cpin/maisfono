b40218c1227a97259f91493af792261617663630
juniorpires <juniorpiresupe@gmail.com>
2017-09-15 06:43:32 -0300

    adicionando botão de login google

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,4 +12,11 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
+  signOut() {
+    //var auth2 = gapi.auth2.getAuthInstance();
+    //auth2.signOut().then(function () {
+    //  console.log('User signed out.');
+    //});
+  }
+
 }
