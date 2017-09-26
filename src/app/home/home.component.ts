cb9a3d253305236384b783e527efff7b05de946d
Wylianne <wyliannec@gmail.com>
2017-09-25 22:01:14 -0300

    atualização no login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  img: String;
+  name: String;
   constructor() { }
 
   ngOnInit() {
+    
+    this.img = localStorage.getItem("img");
+    this.name = localStorage.getItem("name");
   }
 
 }
+
