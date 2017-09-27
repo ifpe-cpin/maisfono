9e0897e4bafd98f36ec9a68de5805f2c2a93c986
Wylianne <wyliannec@gmail.com>
2017-09-27 09:51:36 -0300

    --

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
-    
+    if (typeof(Storage) !== "undefined") {
+        this.router.navigate(['home/dash/']); 
+    }
   }
 
 }
