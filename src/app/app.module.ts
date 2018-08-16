2c6de9412dbfb4606182baa39af5a34b8e12572d
juniorpires <juniorpiresupe@gmail.com>
2018-08-15 22:55:23 -0300

    refactor: instruções comentadas removidas #15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,19 +43,11 @@ import { GridViewComponent } from './grid-view/grid-view.component';
 // Configs 
 export function getAuthServiceConfigs() {
   let config = new AuthServiceConfig(
-      [
-        // {
-        //   id: FacebookLoginProvider.PROVIDER_ID,
-        //   provider: new FacebookLoginProvider("Your-Facebook-app-id")
-        // },
+      [,
         {
           id: GoogleLoginProvider.PROVIDER_ID,
           provider: new GoogleLoginProvider("449875052890-b486rnik4snau6n34lk28940kh9lt1dj.apps.googleusercontent.com")
         },
-        //  {
-        //   id: LinkedinLoginProvider.PROVIDER_ID,
-        //   provider: new GoogleLoginProvider("Your-Linkedin-Client-Id")
-        // },
       ]
   );
   return config;
