ee6d4cd0b3e89b5e435050f303890e65b1216681
juniorpires <juniorpiresupe@gmail.com>
2018-08-22 17:04:02 -0300

    refactor: criado core module para os serviços que serão usados globalmente

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from 'angular5-social-auth';
+import { AngularFireModule } from 'angularfire2';
+import { firebaseConfig } from '../../firebase-config';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+  ],
+  declarations: [],
+  providers:[
+    AuthGuard,
+    AuthService,
+  ]
+})
+export class CoreModule { }
