c40163710145e911595143d7887416bba4c276e3
juniorpires <juniorpiresupe@gmail.com>
2018-10-04 21:02:08 -0300

    configurando o deploy com o Travis

--- deploy.sh
+++ deploy.sh
@@ -32,4 +32,4 @@ git commit -m "Deploy to GitHub Pages"
 # gh-pages será perdido, pois vamos substituí-lo.)  Redirecionamos qualquer saída para
 # /dev/null para ocultar quaisquer dados de credenciais sensíveis que de outra forma possam ser expostos.
 # tokens GH_TOKEN e GH_REF serão fornecidos como variáveis de ambiente Travis CI
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
