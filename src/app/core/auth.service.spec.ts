917cfc961eac3b0f7b3dbec5fa944a98af9cb3fe
juniorpires <juniorpiresupe@gmail.com>
2018-10-06 21:14:05 -0300

    criado arquivo de teste para AuthService

---
+++ src/app/core/auth.service.spec.ts
@@ -0,0 +1,29 @@
+//  import { TestBed, async, inject } from '@angular/core/testing';
+
+// import { AuthService } from './auth.service';
+// import { AngularFireAuth } from 'angularfire2/auth';
+// import { AngularFirestore } from 'angularfire2/firestore';
+// import { Router } from '@angular/router';
+// import { RouterStub } from './router.stub';
+// import { AngularFirestoreStub } from './angularfirestore.stub';
+// import { AngularFireauthStub } from './angularfireauth.stub';
+// import { FirebaseAppConfig, FirebaseOptions } from '@angular/fire';
+
+
+
+//  describe('AuthService', () => {
+//    beforeEach(() => {
+//      TestBed.configureTestingModule({
+//        providers: [AuthService,
+//         AngularFireAuth,
+//         AngularFirestore,
+//          //{provide:AngularFireAuth,useClass:AngularFireauthStub},
+//          //{provide:AngularFirestore,useClass:AngularFirestoreStub},
+//          {provide:Router,useClass:RouterStub}]
+//      });
+//    });
+
+//    it('should ...', inject([AuthService], (service: AuthService) => {
+//      expect(service.googleLogin()).toBeTruthy();
+//    }));
+//  });
