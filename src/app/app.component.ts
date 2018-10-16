36f779578d72f1c52fbd12ce13020cf6eeb56e85
juniorpires <juniorpiresupe@gmail.com>
2018-10-15 22:11:06 -0300

    fix: redirecionando para o dash caso o usuário esteja logado #26

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AuthService } from './core/auth.service';
+import { Observable } from 'rxjs';
+import { tap, map, take } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[AuthService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+
+  constructor(private auth: AuthService,  private router: Router){}
+
+  ngOnInit(){
+    this.auth.user.subscribe( user =>{
+      this.router.navigate(['sistema/dash']);
+})
+  }
 }
