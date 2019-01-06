2d7278e979f1f61f0629e8c900791393278db2b2
Júnior Pires <juniorpiresupe@gmail.com>
2019-01-06 19:06:26 -0300

    Update index.php
    Alterando permissão de acesso para qualquer domínio

--- Slim/public/index.php
+++ Slim/public/index.php
@@ -54,7 +54,7 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
-            ->withHeader('Access-Control-Allow-Origin', 'https://maisfono.com')
+            ->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
