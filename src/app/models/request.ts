084cb2921bcc53a32fd92eac551c7fde4bdd810e
VictorCardoso22 <victor.eacd@gmail.com>
2019-01-28 23:21:51 -0300

    atualizando request url

--- src/app/models/request.ts
+++ src/app/models/request.ts
@@ -1,5 +1,4 @@
 //export const REQUEST_BASE_URL = 'https://localhost/maisfono/Slim/public/index.php/v1'
-// export const REQUEST_BASE_URL = 'https://jrpires.com/mais-fono-rest/public/index.php/v1'
-export const REQUEST_BASE_URL = 'http://localhost/maisfono/Slim/public/index.php/v1'
+export const REQUEST_BASE_URL = 'https://jrpires.com/mais-fono-rest/public/index.php/v1'
 
 export const HOST_URL = 'https://jrpires.com/mais-fono-rest/public/index.php'
