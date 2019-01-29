337e2c53c42ac62d16dc261f0e3504989a55a19d
Junior Pires <juniorpiresupe@gmail.com>
2019-01-29 20:41:39 -0300

    alterado deploy para modo production

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e # termina o script com um código diferente de 0 se alguma coisa falhar
 
 # roda o script de build da nossa aplicação
-ng build --base-href=.
+ng build --prod --base-href=.
 
 # pull requests e commits para outras branches diferentes da master 
 # não devem fazer o deploy, isso é opcional caso queira deletar as próximas 6 linhas
