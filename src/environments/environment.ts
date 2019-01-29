40c174f301db76f208bbf5e4f7b168e1a9272fb2
Junior Pires <juniorpiresupe@gmail.com>
2019-01-29 20:41:16 -0300

    alterando valores de variáveis no environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  request_base_url: 'https://localhost/maisfono/Slim/public/index.php/v1',
-  host_url_server: 'https://localhost/maisfono/Slim/public/index.php',
+  request_base_url: 'https://jrpires.com/mais-fono-rest/public/index.php/v1',
+  host_url_server: 'https://jrpires.com/mais-fono-rest/public/index.php',
   host_url_client: 'https://localhost:4200',
   pusher: {
     key: '527a8eb84680c6505dbe',
