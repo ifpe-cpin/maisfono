d538bd058a684bcd866a0f5c4e12bf2ca1d86104
VictorCardoso22 <victor.eacd@gmail.com>
2019-02-16 08:55:57 -0300

    corrigido erro nos dashs

--- Slim/public/index.php
+++ Slim/public/index.php
@@ -1725,6 +1725,7 @@ function getConnection() {
     $dbpass="maisfono_0001";
     $dbname="jrpiresc_maisfono_rest";
     
+    
     $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $dbh;
